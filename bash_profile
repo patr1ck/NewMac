5f9901ee10a8d01e1c293edd61138cb7cd25c406
Patrick Gibson <patrick@Hedera.local>
2014-10-19 22:11:24 -0700

    Initial commit.

---
+++ bash_profile
@@ -0,0 +1,22 @@
+# Color
+export CLICOLOR=1
+
+# Macports
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+
+# rbenv
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+# Prompt
+function parse_git_branch {
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo " ["${ref#refs/heads/}"]"
+}
+
+function ruby_version {
+  echo " (`rbenv version-name`)"
+}
+
+export PS1="\u@\H \w\$(parse_git_branch)\$(ruby_version) \n\$ "
+ 
---
+++ setup-new-machine.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# ------- Bash profile --------
+mv ~/.bash_profile ~/.bash_profile.original
+mv ./bash_profile ~/.bash_profile
+
+
+# ------- Finder / UI ---------
+
+# Change the dock effect to not be ugly
+defaults write com.apple.Dock mineffect scale
+
+# Show the friggen library folder
+chflags nohidden ~/Library/
+
+# Automatically search the current folder instead of the system
+defaults write com.apple.finder FXDefaultSearchScope -string “SCcf”
+
+# Show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Set sidebar icon size to medium
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+
+# Sort desktop icons by Date Added
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy dateAdded" ~/Library/Preferences/com.apple.finder.plist
+
+# Add iOS Simulator to Launchpad
+sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone Simulator.app" "/Applications/iOS Simulator.app"
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+
+# ------- Safari -------
+
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+
+
+# ------- Restart Stuff -------
+
+killall Finder
