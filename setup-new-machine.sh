d25a2fb6ebdf42c61268ab0415a5786a9753c56d
Patrick B. Gibson <patrick@fadeover.org>
2015-12-04 12:12:10 -0800

    More Safari and Tweetbot prefs.

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -57,7 +57,14 @@ defaults write com.apple.dashboard mcx-disabled -bool true
 
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari AutoFillPasswords -bool false
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
+defaults write com.apple.Safari ShowStatusBar -bool true
 
+
+# ------- Tweetbot -------
+defaults write com.tapbots.TweetbotMac OpenURLsDirectly YES
 
 # ------- Restart Stuff -------
 killall Finder Dock Safari
